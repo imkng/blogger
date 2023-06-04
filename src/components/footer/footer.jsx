b17e7106c3317b95ac66f24c77085ccb97af5eda
Nand <1india.kng@gmail.com>
2023-06-05 00:38:34 +0530

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from './footer.module.css'
 
@@ -5,7 +6,11 @@ const Footer = () => {
   return (
     <div className={styles.container}>
         <div>©2023 Blogger. All rights reserved.</div>
-        <div className={styles.container}>   
+        <div className={styles.social}>
+          <Image src='/1.png' height={15} width = {15} className={styles.icon}/>
+          <Image src='/2.png' height={15} width = {15} className={styles.icon}/>
+          <Image src='/3.png' height={15} width = {15} className={styles.icon}/>
+          <Image src='/4.png' height={15} width = {15} className={styles.icon}/>
         </div>
     </div>
   )
