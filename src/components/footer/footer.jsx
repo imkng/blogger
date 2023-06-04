6eba7cbcccb517ed00e6f423f38d13d565d6d58d
Nand <1india.kng@gmail.com>
2023-06-05 00:23:31 +0530

    navbar

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div>©2023 Blogger. All rights reserved.</div>
+        <div className={styles.container}>   
+        </div>
+    </div>
+  )
+}
+
+export default Footer
