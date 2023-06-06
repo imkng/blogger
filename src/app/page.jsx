f377851fbcf5df6a9c21538ce025f582e34ff9b1
Nand <1india.kng@gmail.com>
2023-06-06 23:29:16 +0530

    HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your didital products
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
