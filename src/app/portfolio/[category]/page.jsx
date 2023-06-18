abedd64c36643a51aa17e0251ad468a92a0f83ff
Nand <1india.kng@gmail.com>
2023-06-18 22:21:58 +0530

    portfolio

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+import React from "react";
+import styles from "./page.module.css";
 
-const Category = () => {
+const Category = ({ params }) => {
+  console.log(params);
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button text="See More" url = "#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill = {true}
+            src = "https://images.pexels.com/photos/17170319/pexels-photo-17170319/free-photo-of-landscape-field-summer-sun.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button text="See More" url = "#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill = {true}
+            src = "https://images.pexels.com/photos/17170319/pexels-photo-17170319/free-photo-of-landscape-field-summer-sun.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button text="See More" url = "#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            fill = {true}
+            src = "https://images.pexels.com/photos/17170319/pexels-photo-17170319/free-photo-of-landscape-field-summer-sun.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
